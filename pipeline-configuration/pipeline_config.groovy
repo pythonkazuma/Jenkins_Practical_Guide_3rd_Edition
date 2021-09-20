5e42ef6d930807955ea39d785417cb6157846dde
pythonkazuma <mamewawe500@hotmail.co.jp>
2021-09-20 12:56:05 +0900

    Update pipeline_config.groovy

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,19 +1,10 @@
-jte{
-    allow_scm_jenkinsfile = true
-}
-
-@merge libraries{
-    sonarqube
-    ansible
-}
- 
-
-application_environments{
-    dev{
-        ip_addresses = [ "0.0.0.1", "0.0.0.2" ]
-    }
-    prod{
-        long_name = "Production"
-        ip_addresses = [ "0.0.1.1", "0.0.1.2", "0.0.1.3", "0.0.1.4" ]
+libraries{
+    maven
+    //gradle
+    //sonarqube
+    
+    sonarqube{
+      scanner_version = "SonarScanner 3.0"
+      enforce_quality_gate = false 
     }
 }
